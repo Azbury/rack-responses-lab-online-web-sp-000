ed0e54daa69375e7bb1d7cbc6666db22d564bcdd
Austin Asbury <acasbury25@aol.com>
2019-09-29 18:25:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    if Time.now > 12
+    if Time.hour > 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
